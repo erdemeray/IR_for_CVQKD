8524bcc0ee0e94c597b700ba6146e2093b5f348b
erdem.cil <erdem.cil@kit.edu>
2024-06-10 18:48:56 +0200

    doc compiler fix 5

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,8 +16,8 @@ read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 breathe_projects = {}
 
 if read_the_docs_build:
-    input_dir = '../IR_lib'
-    output_dir = 'build'
+    input_dir = '../../IR_lib'
+    output_dir = '../build'
     configureDoxyfile(input_dir, output_dir)
     subprocess.call('doxygen', shell=True)
     breathe_projects['information_reconciliation'] = output_dir + '/xml'
